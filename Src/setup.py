78a48a9cd2be297de957e9122d026232777f1237
MXPServer 20953 onetechguy <73059135+MXP2095onetechguy@users.noreply.github.com>
2021-11-03 19:22:16 +0700

    Update setup.py to include more information

--- Src/setup.py
+++ Src/setup.py
@@ -8,20 +8,33 @@ if sys.platform == "win32":
 
 executables = [cxF.Executable("TkEdit.py", base=base, target_name="TkEdit.exe", icon="asset/TkEditI.ico")]
 
-includefile = ["asset/", "LICENSE", "clearlooks/", "Pmw/", "list.txt", "awthemes/", "themes/"]
+includefile = ["asset/", "LICENSE", "clearlooks/", "Pmw/", "list.txt", "awthemes/", "ttkthemes/"]
 
 include = ["atexit"]
 
 exclude = []
 
-pkgmodule = ["PIL", "requests", "tkinterdnd2", "witkets", "ttkwidgets"]
+pkgmodule = ["PIL", "requests", "tkinterdnd2", "witkets", "ttkwidgets", "tksvg"]
 
 options = {"build_exe": {"includes":include, "packages": pkgmodule, 'include_files':includefile, 'excludes': exclude}}
+
+f = open("LICENSE", "r")
+License = f.read()
+f.close()
+f = None
 
 cxF.setup(
     name="TkEdit",
     version="0.1",
+    url="https://mxp2095onetechguy.github.io/TkEdit/",
+    license=License,
+    author="MXPSQL",
+    author_email="2000onetechguy@gmail.com",
     description="Tkinter (Tk) based editor. Better than Microsoft notepad",
+    platforms=["Windows", "Linux", "MacOS"],
+    keywords=["Tkinter", "Editor"],
+
+
     options=options,
     executables=executables,
 )
